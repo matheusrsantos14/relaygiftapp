d72b9d1abe43c0b928eb00f72fa5ae54b4028b1d
Matheus Santos <matheusrsantos14@gmail.com>
2019-06-20 22:44:39 -0400

    initial app

---
+++ db/migrate/20190620051636_add_subscriber_role_to_spree_roles.spree_account_recurring.rb
@@ -0,0 +1,8 @@
+# This migration comes from spree_account_recurring (originally 20131202110012)
+class AddSubscriberRoleToSpreeRoles < ActiveRecord::Migration[4.2]
+  def up
+  end
+
+  def down
+  end
+end
